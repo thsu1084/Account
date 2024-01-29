05a19a1f15fac3df533598b3623fe5238e33b884
thsu1084 <98458216+thsu1084@users.noreply.github.com>
2024-01-29 18:59:09 +0900

    Update TransactionService.java

--- src/main/java/com/example/Account/service/TransactionService.java
+++ src/main/java/com/example/Account/service/TransactionService.java
@@ -1,5 +1,6 @@
 package com.example.Account.service;
 
+
 import com.example.Account.db.Transaction;
 import com.example.Account.model.TransactionDto;
 import java.util.List;
@@ -7,11 +8,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 public interface TransactionService {
 
-    TransactionDto useBalance(Long id, String accountNumber, Long amount);
+    TransactionDto useBalance(String token,Long id, String accountNumber, Long amount);
 
     TransactionDto FailToTransaction(String accountNumber , Long amount);
 
-    TransactionDto cancelBalance(String transactionId,String accountNumber, Long amount);
+    TransactionDto cancelBalance(String token,String transactionId,String accountNumber, Long amount);
 
      TransactionDto getTransaction(String transactionId);
 }
