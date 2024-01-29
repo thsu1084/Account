58acb0f34d0ae1a85177a5b1b47c9bd20d9da098
thsu1084 <98458216+thsu1084@users.noreply.github.com>
2024-01-29 18:58:01 +0900

    Update TransactionServiceImpl.java

--- src/main/java/com/example/Account/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/example/Account/service/impl/TransactionServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.Account.service.impl;
 
+
 import static com.example.Account.common.enums.AccountStatus.UNREGISTERED;
 import static com.example.Account.common.enums.TransactionResultType.FAIL;
 import static com.example.Account.common.enums.TransactionResultType.SUCCESS;
@@ -10,6 +11,8 @@ import static com.example.Account.common.enums.TransactionType.USE_CANCELLED;
 
 import com.example.Account.common.error.ErrorCode;
 import com.example.Account.common.exception.ApiException;
+import com.example.Account.config.security.JwtAuthenticationFilter;
+import com.example.Account.config.security.JwtTokenProvider;
 import com.example.Account.db.Account;
 import com.example.Account.db.Transaction;
 import com.example.Account.db.User;
@@ -39,32 +42,54 @@ public class TransactionServiceImpl implements TransactionService {
     private final AccountRepository accountRepository;
     @Autowired
     private final TransactionRepository transactionRepository;
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
 
     public TransactionServiceImpl(UserRepository userRepository,AccountRepository accountRepository,
-        TransactionRepository transactionRepository) {
+        TransactionRepository transactionRepository, JwtTokenProvider jwtTokenProvider) {
         this.userRepository = userRepository;
         this.accountRepository = accountRepository;
         this.transactionRepository = transactionRepository;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @Override
-    public TransactionDto useBalance(Long id, String accountNumber, Long amount) {
+    public TransactionDto useBalance(String token,Long id, String accountNumber, Long amount) {
 
-        User user = userRepository.findById(id)
-            .orElseThrow(()->new ApiException(ErrorCode.USER_NOT_FOUND));
+        log.info("token : {}",token);
+
+        token = token.substring(JwtAuthenticationFilter.TOKEN_PREFIX.length());
+
+        String uid = jwtTokenProvider.getUsername(token);
+
+        log.info("uid : {}",uid);
+
+        User user = userRepository.findByUid(uid)
+            .orElseThrow(()-> new ApiException(ErrorCode.USER_NOT_FOUND));
+
+        log.info("user : {} , id {}",user,id);
+
+        if (!Objects.equals(user.getId(),id)){
+            log.info("사용자가 일치하지 않습니다.");
+            throw new ApiException(ErrorCode.USER_NOT_FOUND);
+        }
 
         Account account = accountRepository.findByAccountNumber(accountNumber)
             .orElseThrow(()->new ApiException(ErrorCode.THE_ACCOUNT_DOES_NOT_EXIST));
 
         if (account.getAccountStatus().equals(UNREGISTERED)){
 
-            return FailToTransaction(accountNumber,amount);    
+            log.info("계좌가 해지상태 입니다.");
+
+            return FailToTransaction(accountNumber,amount);
+
         }
 
         if (account.getBalance() < amount){
-            
+
+            log.info("잔액이 부족합니다.");
             return FailToTransaction(accountNumber,amount);
-        
+
         }
 
 
@@ -128,39 +153,61 @@ public class TransactionServiceImpl implements TransactionService {
 
 
     @Override
-    public TransactionDto cancelBalance(String transactionId, String accountNumber, Long amount)
+    public TransactionDto cancelBalance(String token,String transactionId, String accountNumber, Long amount)
     throws RuntimeException{
 
-        Transaction transaction = transactionRepository.findByTransactionId(transactionId)
-            .orElseThrow(()->new ApiException(ErrorCode.BAD_REQUEST));
+        log.info("token : {}",token);
+
+        token = token.substring(JwtAuthenticationFilter.TOKEN_PREFIX.length());
+
+        String uid = jwtTokenProvider.getUsername(token);
+
+        log.info("uid : {}",uid);
+
+        User user = userRepository.findByUid(uid)
+            .orElseThrow(()-> new ApiException(ErrorCode.USER_NOT_FOUND));
 
         Account account = accountRepository.findByAccountNumber(accountNumber)
             .orElseThrow(()->new ApiException(ErrorCode.THE_ACCOUNT_DOES_NOT_EXIST));
 
-        // 이미 취소된 거래 일 떄
+        log.info("User : {} , Account : {}",user,account);
+
+        if (!Objects.equals(user.getId(),account.getUser().getId())){
+            log.info("사용자가 일치하지 않습니다.");
+            throw new ApiException(ErrorCode.USER_NOT_FOUND);
+        }
+
+        Transaction transaction = transactionRepository.findByTransactionId(transactionId)
+            .orElseThrow(()->new ApiException(ErrorCode.BAD_REQUEST));
+
+        log.info("Transaction : {}",transaction);
+
         if (transaction.getTransactionType().equals(USE_CANCELLED)){
+            log.info("이미 취소된 거래");
             throw new ApiException(ErrorCode.TRANSACTION_HAS_ALREADY_BEEN_CANCELED);
         }
 
-        // 실패한 거래 일 때
         if (transaction.getTransactionResultType().equals(FAIL)){
+            log.info("실패한 거래");
             throw new ApiException(ErrorCode.TRANSACTION_HAS_FAILED);
         }
 
-        // 거래 계좌 아이디와 계좌 아이디가 다를 때
         if (!Objects.equals(transaction.getAccount().getId(),account.getId())){
+            log.info("거래 계좌 아이디와 계좌 아이디가 다를 때");
             throw new ApiException(ErrorCode.TRANSACTION_ACCOUNT_ID_DOES_NOT_MATCH);
         }
 
         log.info("transaction : {} {}",transaction.getAmount(),amount);
 
-        // 거래 금액과 사용자가 요구하는 금액이 다를 때
+
         if (!Objects.equals(transaction.getAmount(),amount)){
+            log.info("거래 금액과 사용자가 요구하는 금액이 다를 때");
             throw new ApiException(ErrorCode.AMOUNT_DOES_NOT_MATCH);
         }
 
-        // 1년이 지난 거래일 때
+
         if( transaction.getTransactedAt().isBefore(LocalDateTime.now().minusYears(1))){
+            log.info("1년이 지난 거래일 때");
             throw new ApiException(ErrorCode.TRANSACTION_IS_OLDER_THAN_1_YEAR);
         }
 
