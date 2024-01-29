9aeec0cda6f977037f0e6bf0b298170ce03fe6f0
thsu1084 <98458216+thsu1084@users.noreply.github.com>
2024-01-29 19:08:49 +0900

    Update AccountController.java

--- src/main/java/com/example/Account/controller/AccountController.java
+++ src/main/java/com/example/Account/controller/AccountController.java
@@ -1,6 +1,5 @@
 package com.example.Account.controller;
 
-
 import com.example.Account.model.AccountDto;
 import com.example.Account.model.CreateAccountDto;
 import com.example.Account.model.DeleteAccountDto;
@@ -13,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,26 +25,24 @@ public class AccountController {
     @Autowired
     private final AccountServiceImpl accountServiceImpl;
 
-    @ApiImplicitParams({
-        @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 발급 받은 access_token", required = true, dataType = "String", paramType = "header")
-    })
+
     @PostMapping("/create")
     public CreateAccountDto createAccount(
+         @RequestHeader(name = "Authorization") String token,
          @RequestParam Long id,
          @RequestParam Long initialBalance
     ){
-        return accountServiceImpl.createAccount(id, initialBalance);
+        return accountServiceImpl.createAccount(token,id, initialBalance);
     }
 
-    @ApiImplicitParams({
-        @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 발급 받은 access_token", required = true, dataType = "String", paramType = "header")
-    })
+
     @DeleteMapping("/delete")
     public DeleteAccountDto deleteAccount(
+        @RequestHeader(name = "Authorization") String token,
         @RequestParam Long id,
         @RequestParam String accountNumber
     ){
-        return accountServiceImpl.deleteAccount(id, accountNumber);
+        return accountServiceImpl.deleteAccount(token,id, accountNumber);
     }
 
     @GetMapping("/account")
